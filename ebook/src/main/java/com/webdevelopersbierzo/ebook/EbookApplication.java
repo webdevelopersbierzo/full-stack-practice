b9ea09aaa666e0e36f2a51c4bf34927a3dc574bc
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2023-02-16 11:13:37 +0100

    add log

--- ebook/src/main/java/com/webdevelopersbierzo/ebook/EbookApplication.java
+++ ebook/src/main/java/com/webdevelopersbierzo/ebook/EbookApplication.java
@@ -1,10 +1,13 @@
 package com.webdevelopersbierzo.ebook;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EbookApplication {
+	private static final Logger logger = LoggerFactory.getLogger(EbookApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(EbookApplication.class, args);
