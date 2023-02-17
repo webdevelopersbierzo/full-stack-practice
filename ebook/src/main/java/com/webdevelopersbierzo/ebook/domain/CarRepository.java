5b125365938fb65939a240d2664b267a3a481228
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2023-02-17 07:40:22 +0100

    actualizado los metodos Restfull

--- ebook/src/main/java/com/webdevelopersbierzo/ebook/domain/CarRepository.java
+++ ebook/src/main/java/com/webdevelopersbierzo/ebook/domain/CarRepository.java
@@ -1,21 +1,23 @@
 package com.webdevelopersbierzo.ebook.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
-
+@RepositoryRestResource
 public interface CarRepository extends JpaRepository<Car, Long> {
 
     //Fetch cars by brand
-    List<Car> findByBrand(String brand);
+    List<Car> findByBrand(@Param("brand")String brand);
     //Fetch cars by color
-    List<Car> findByColor(String color);
+    List<Car> findByColor(@Param("color")String color);
     // Fetch cars by year
-    List<Car> findByYear(Integer year);
+    List<Car> findByYear(@Param("year")Integer year);
     // Fetch cars by brand and model
-    List<Car> findByBrandAndModel(String brand, String model);
-    //Fetch cars by brand and color
-    List<Car> findByBrandOrColor(String brand, String color);
+//    List<Car> findByBrandAndModel(@Param("brand")@Param("model") String brand, String model);
+//    //Fetch cars by brand and color
+//    List<Car> findByBrandOrColor(@Param("brand", "model") String brand, String color);
 
 
 
